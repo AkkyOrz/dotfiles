45846c13d487c9743d85da6e4c8b2073b0747048
Akihide Ito <akky.supernova@gmail.com>
2021-01-17 15:09:12 +0900

    feat: add some PATHs variable

--- .zsh.d/10-path.zsh
+++ .zsh.d/10-path.zsh
@@ -1,6 +1,14 @@
 # PATH
-export PATH="$HOME/.local/bin:$PATH"
-export PATH="$HOME/.rbenv/bin:$PATH"
-export PATH="$PATH:/mnt/c/Program Files/Docker/Docker/resources/bin" # https://github.com/docker/compose/issues/7495
-export PATH="$HOME/protoc3/bin:$PATH"
+export PATH="$PATH:$HOME/.local/bin"
+export PATH="$PATH:$HOME/.rbenv/bin"
+export PATH="$PATH:$HOME/.ebcli-virtual-env/executables"
+export PATH="$PATH:$HOME/protoc3/bin"
+export PATH="$PATH:$HOME/Android/Sdk/platform-tools"
+export PATH="$PATH/usr/local/go/bin"
+export PATH="$PATH:$(go env GOPATH)/bin"
+export PATH="$PATH:$HOME/.npm-global/bin"
+export PATH="$PATH:/usr/local/texlive/2019/bin/x86_64-linux"
+export PATH="$PATH:$HOME/.cargo/bin"
+export PATH="$PATH:$HOME/.local/bin"
+export PATH="$PATH:/opt/td-agent/embedded/bin/"
 
